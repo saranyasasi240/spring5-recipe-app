bef43c11a98bb36f52435d404b0333441d996f5c
Prasad <saranyasasi240@gmail.com>
2024-10-22 10:30:15 +0530

    Added methods for getting data from DB by using Spring Data JPA

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,33 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+public class IndexController {
+
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"", "/", "index"})
+    public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        System.out.println("Category Id id: " + categoryOptional.get().getId());
+        System.out.println("Unit of measure id is: " + unitOfMeasureOptional.get().getId());
+        return "index";
+    }
+}
