83cec9e4f2305a4dd0579179d7372b0a887eb5b2
Prasad <saranyasasi240@gmail.com>
2024-10-28 10:16:28 +0530

    Added the functionality for the recipe listing and recipe view.

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -25,7 +26,7 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void getRecipe(){
+    public void getRecipe() {
         Recipe recipe = new Recipe();
         HashSet recipeData = new HashSet();
         recipeData.add(recipe);
@@ -34,6 +35,21 @@ public class RecipeServiceImplTest {
 
         Set<Recipe> recipeSet = recipeService.getRecipes();
         assertEquals(recipeSet.size(), 1);
-        verify(recipeRepository,times(1)).findAll();
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void getRecipeById() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        Recipe expectedRecipe = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", expectedRecipe);
+
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
     }
 }
