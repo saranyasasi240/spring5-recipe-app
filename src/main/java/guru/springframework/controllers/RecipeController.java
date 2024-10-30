583ebc5ff7c545223a49a0cefe5d93f919f853ed
Prasad <saranyasasi240@gmail.com>
2024-10-30 15:26:30 +0530

    Exception Handling | Global exception handling using @ControllerAdvice

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -61,15 +61,4 @@ public class RecipeController {
         modelAndView.addObject("exception", exception);
         return modelAndView;
     }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormatError(Exception exception) {
-        log.error("Handing Number Format Exception");
-        log.error(exception.getMessage());
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-        return modelAndView;
-    }
 }
